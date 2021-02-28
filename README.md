0b3f407b115679f95d42f43041d77a0610f6ebc2
madlymad <madlymad@users.noreply.github.com>
2021-02-28 18:40:58 +0200

    Add comments and readme

---
+++ README.md
@@ -0,0 +1,8 @@
+This python program is designed to extract Greek Covid data from Viber Community to Excel.
+
+Requirements:
+- Windows
+- To have installed ViberPC
+- Be part of Viber Community [Ελληνική Κυβέρνηση - Επίσημη Ενημέρωση για COVID-19](https://invite.viber.com/?g2=AQAVNiDVlfjrIEtaYLf1s2sUzRrpfLVlfLVg4J8wkdNKMUSnUcQWJxnXH0Os1heH)
+- In case you want all history data you have to scroll in this conversation all the way to the top.
+ 
--- data.py
+++ data.py
@@ -1,26 +1,33 @@
 
 from datetime import datetime
 
-NOTHING = 'κανένα'
+ZERO_TEXT = 'κανένα'
 
+# Convert data line and handle special cases
 def specialHandler(data):
-    if NOTHING in data:
-        data[data.index(NOTHING)] = 0
+    # Convert none text to number
+    if ZERO_TEXT in data:
+        data[data.index(ZERO_TEXT)] = 0
 
+    # Clear numbers from dots
     for i in range(len(data)):
         data[i] = numbers(data[i])
 
+    # Convert special dates that not fit the rest of data
     if data[0] == '21/9/2020':
         data.pop(2) # delete 184
     if data[0] == '8/6/2020':
         data.pop(3) # delete 4
     
+    # Format dates
     data[0] = dateFormat(data[0])
 
+    # Move tests to proper location when ΜΕΘ is missing
     if len(data) == 7 and int(data[-1]) > 50000:
         data.insert(-1, None)
     
     #print(f"\t\t{data}")
+    # Fill rest columns with None
     return data + [None]*(9-len(data))
     
 def numbers(s):
